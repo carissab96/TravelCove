743c6ab529f07fb265d9442d3ae7bf812ce7b93f
carissab96 <bayscarissa706@gmail.com>
2024-12-13 01:49:19 -0600

    add user model scope for user secdurity

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,70 +1,85 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 
+const { Model, Validator } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
   }
-  User.init({
-    username: { 
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        len: {
-          args: [4, 30],
-          msg: "Username must be between 4 and 30 characters."
+
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error('Cannot be an email.');
+            }
+          },
         },
-        isNotEmail(value) {
-          const isEmail = require('validator/lib/isEmail');
-          if (isEmail(value)) {
-            throw new Error('Username cannot be an email address');
-          }
-        }
-      }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 256],
+          isEmail: true,
+        },
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60],
+        },
+      },
     },
-
-    email: {
-      type: DataTypes.STRING, 
-      allowNull: false,
-      unique: true,
-      isEmail: true,
-      validate: {
-        len: {
-          args: [3, 256],
-          msg: "Email must be between 3 and 256 characters."
-        }
-      }
-    },
-
-    hashedPassword: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: {
-          args: [60, 60],
-          msg: "String length must be exactly 60 characters."
-        }
-      }
-    },
-  }, {
-    sequelize,
-    modelName: 'User',
-    defaultScope: {
-      attributes: {
-        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
-      }
+    {
+      sequelize,
+      modelName: 'User',
+      defaultScope: {
+        attributes: {
+          exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'],
+        },
+      },
     }
-  });
+  );
   return User;
 };
+
+/*The backend login flow in this project will be based on the following plan:
+
+1. The API login route will be hit with a request body holding a valid
+   credential (either username or email) and password combination.
+2. The API login handler will look for a `User` with the input credential in
+   either the `username` or `email` columns.
+3. Then the `hashedPassword` for that found `User` will be compared with the
+   input `password` for a match.
+4. If there is a match, the API login route should send back a JWT in an
+   HTTP-only cookie and a response body. The JWT and the body will hold the
+   user's `id`, `username`, and `email`.
+
+The backend sign-up flow in this project will be based on the following plan:
+
+1. The API signup route will be hit with a request body holding a `username`,
+   `email`, and `password`.
+2. The API signup handler will create a `User` with the `username`, an `email`,
+   and a `hashedPassword` created from the input `password`.
+3. If the creation is successful, the API signup route should send back a JWT in
+   an HTTP-only cookie and a response body. The JWT and the body will hold the
+   user's `id`, `username`, and `email`.
+
+The backend logout flow will be based on the following plan:
+
+1. The API logout route will be hit with a request.
+2. The API logout handler will remove the JWT cookie set by the login or signup
+   API routes and return a JSON success message. */
+
+   
